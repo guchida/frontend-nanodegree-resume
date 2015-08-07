d8a748e784bd7f9c3363574f316ab03b6e0e0ba0
Greydon Uchida <guchida@ifwe.co>
2015-08-06 22:39:11 -0700

    Update formatting in bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,24 @@
 var bio = {
-    name:"Greydon Uchida",
-    role:"Software Engineer",
-    cell:"(415)555-1234",
-    email:"Email:g@greydon.org",
-    location:"San Francisco Bay Area",
-    pictureURL:"images/image.png",
-    welcome:"This is my resume.",
-    skills:"Jenkins, unix, mac, python, devops"
+    "name": "Greydon Uchida",
+    "role": "Software Engineer",
+    "contacts":{
+        "cell": "(415)555-1234",
+        "email": "Email:g@greydon.org",
+        "location": "San Francisco Bay Area"
+    },
+    "pictureURL": "images/image.png",
+    "welcome": "This is my resume.",
+    "skills": ["Jenkins", "unix", "mac", "python", "devops"]
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedCell = HTMLmobile.replace("%data%", bio.cell);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+var formattedCell = HTMLmobile.replace("%data%", bio.contacts.cell);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 var formattedPicture = HTMLbioPic.replace("%data%", bio.pictureURL);
 var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
@@ -24,3 +27,5 @@ $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedLocation);
 $("#topContacts").append(formattedPicture);
 $("#topContacts").append(formattedWelcome);
+$("#topContacts").append(HTMLskillsStart);
+$("#topContacts").append(formattedSkills);
