8578b50ddecdd9b5722fce8bf54b75554cb2c25d
Greydon Uchida <guchida@ifwe.co>
2015-08-08 22:16:04 -0700

    Add work and education objects, clean up resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,31 +1,29 @@
 var bio = {
-    "name": "Greydon Uchida",
-    "role": "Software Engineer",
-    "contacts":{
-        "cell": "(415)555-1234",
-        "email": "Email:g@greydon.org",
-        "location": "San Francisco Bay Area"
+    "name" : "Greydon Uchida",
+    "role" : "Software Engineer",
+    "contacts" : {
+        "mobile" : "(415)555-1234",
+        "email" : "Email:g@greydon.org",
+        "github" : "https://github.com/guchida",
+        "location" : "San Francisco Bay Area"
     },
-    "pictureURL": "images/image.png",
-    "welcome": "This is my resume.",
-    "skills": ["Jenkins", "unix", "mac", "python", "devops"]
+    "pictureURL" : "images/image.png",
+    "welcome" : "This is my resume.",
+    "skills" : ["Jenkins", "unix", "mac", "python", "devops"]
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedCell = HTMLmobile.replace("%data%", bio.contacts.cell);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedPicture = HTMLbioPic.replace("%data%", bio.pictureURL);
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+var work = {
+    "position" : "Software Engineer",
+    "employer" : "if(we)",
+    "years" : "5",
+    "location" : "San Francisco, CA",
+    "description" : "This is what if(we) does, etc."
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#topContacts").append(formattedCell);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedLocation);
-$("#topContacts").append(formattedPicture);
-$("#topContacts").append(formattedWelcome);
-$("#topContacts").append(HTMLskillsStart);
-$("#topContacts").append(formattedSkills);
+var education = {
+    "school" : "UC Davis",
+    "gradYear" : "2005",
+    "city" : "Davis, CA",
+    "major" : "Computer Science and Engineering",
+    "minor" : "Communication"
+};
