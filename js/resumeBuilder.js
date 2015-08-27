8391b6ff3cbe1f3ead3cecac704ba5a524625981
Greydon Uchida <guchida@ifwe.co>
2015-08-26 18:56:38 -0700

    Add skills and work experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,11 +13,15 @@ var bio = {
 };
 
 var work = {
-    "position" : "Software Engineer",
-    "employer" : "if(we)",
-    "years" : "5",
-    "location" : "San Francisco, CA",
-    "description" : "This is what if(we) does, etc."
+    "jobs" : [
+        {
+            "position" : "Software Engineer",
+            "employer" : "if(we)",
+            "years" : "5",
+            "location" : "San Francisco, CA",
+            "description" : "This is what if(we) does, etc."
+        }
+    ]
 };
 
 var education = {
@@ -27,3 +31,18 @@ var education = {
     "major" : "Computer Science and Engineering",
     "minor" : "Communication"
 };
+
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    bio.skills.forEach(function(skill) {
+        $("#skills").append(HTMLskills.replace("%data%", skill));
+    });
+}
+
+$("#workExperience").append(HTMLworkStart);
+work.jobs.forEach(function(job) {
+    $("#workExperience").append(HTMLworkEmployer.replace("%data%", job.employer));
+    $("#workExperience").append(HTMLworkTitle.replace("%data%", job.position));
+});
